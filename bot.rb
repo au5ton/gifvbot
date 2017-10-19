a4b07845f8ed50b190b7fc8a1453bef6e0fef104
Austin Jackson <austinjckson@gmail.com>
2017-10-19 14:15:15 -0500

    hmm

--- bot.rb
+++ bot.rb
@@ -31,10 +31,22 @@ Telegram::Bot::Client.run(ENV['TELEGRAM_BOT_TOKEN']) do |bot|
     bot_username = bot.api.get_me()["result"]["username"]
     print("#{bot_username} ✅\n")
     bot.listen do |message|
-        puts "doc: #{message.document}, class: #{message.document.class}"
-        puts "\tmime: #{message.document.mime_type}, size: #{message.document.file_size}"
-        puts "\tin_range: #{in_range(message.document.file_size)}"
-        puts "photo: #{message.photo}, class: #{message.photo.class}"
+        #puts "doc: #{message.document}, class: #{message.document.class}"
+        #puts "\tmime: #{message.document.mime_type}, size: #{message.document.file_size}"
+        #puts "\tin_range: #{in_range(message.document.file_size)}"
+        #puts "photo: #{message.photo}, class: #{message.photo.class}"
+
+        if message.document.instance_of? Telegram::Bot::Types::Document
+            puts "mime: #{message.document.mime_type}, size: #{message.document.file_size}"
+            if message.document.mime_type == "image/gif"
+                puts "we gotta gif"
+                puts bot.api.get_file({"file_id" => message.document.file_id}) # doesn't account for ResponseErrors
+            end
+        else
+            puts "that boy aint right"
+        end
+
+
         case message.text
         when '/start'
             bot.api.send_message(chat_id: message.chat.id, text: "Hello, #{message.from.first_name}")
