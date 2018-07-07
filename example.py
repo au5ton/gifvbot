0fb365de5c8869980164cb5d7f923bdf62c7d57d
Austin Jackson <austinjckson@gmail.com>
2018-07-06 22:55:05 -0500

    testing

--- example.py
+++ example.py
@@ -1,11 +1,20 @@
-from pyrogram import Client, Filters
+#!/usr/bin/env python3
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+from pyrogram import Client, Filters, MessageHandler
+import os
 
-app = Client("my_account")
+app = Client(
+    "my_account",
+    api_id=os.getenv("MTPROTO_API_ID"),
+    api_hash=os.getenv("MTPROTO_API_HASH")
+)
 
-
-@app.on_message(Filters.private)
-def hello(client, message):
+def on_document(client, message):
+    #print(client)
+    print(message)
     message.reply("Hello {}".format(message.from_user.first_name))
 
 
+app.add_handler(MessageHandler(on_document, Filters.document))
 app.run()
